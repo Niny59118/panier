578de4616ac027a73df8ce1ef7b98cab836a779b
fanny modiano <fanny13-m@hotmail.fr>
2021-09-22 09:48:13 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Panier from './Panier'
+
+
+export class App extends Component {
+
+  render() {
+
+    return (
+      <div className="container">
+        <Panier />
+      </div>
+
+    )
+  }
+}
+
+export default App
+
---
+++ Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Item extends React.Component {
+  render() {
+    return (
+      <li key={this.props.item.id}>
+        {this.props.item.id} - {this.props.item.productName} - ${this.props.item.price}
+        <button onClick={() => this.props.recevoirItem(this.props.item)}>Panier</button>
+      </li>
+    )
+  }
+}
